9cf01fbb0fbc86ba000cca7c9544657b7a0e1e5a
Varsha Varadarajan <varshasvaradarajan@gmail.com>
2017-09-18 20:25:30 +0530

    Specify jar location.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 pid=$(ps aux | grep "petclinic" | grep "$SERVER_PORT" | awk '{print $2}')
-kill $pid
+if [[ -z "$pid" ]] ; then
+  kill $pid
+fi
+
 echo "Deploying at port: $SERVER_PORT"
-java -jar *.jar --server.port=$SERVER_PORT
+java -jar ../dist/*.jar --server.port=$SERVER_PORT > /dev/null 2>&1 &
