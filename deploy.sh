3be5b3db1c10a539df805f3ae592378a0c6a669f
Aravind SV <arvind.sv@gmail.com>
2017-09-20 10:23:56 -0400

    Show more info

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-pid=$(ps aux | grep "petclinic" | grep "$SERVER_PORT" | awk '{print $2}')
+pid=$(pgrep -f "petclinic.*${SERVER_PORT}")
 if [[ -n "$pid" ]] ; then
-  kill $pid
+  kill "$pid"
 fi
 
 echo "Deploying at port: $SERVER_PORT"
-java -jar ../dist/*.jar --server.port=$SERVER_PORT > /dev/null 2>&1 &
+java -jar ../dist/*.jar --server.port=$SERVER_PORT & sleep 5
