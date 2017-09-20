1430635861e941fab27d9bd089a77d1113f6f4b6
Aravind SV <arvind.sv@gmail.com>
2017-09-20 10:38:03 -0400

    Wait for port to be free before deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-pid=$(pgrep -f "petclinic.*${SERVER_PORT}")
-if [[ -n "$pid" ]] ; then
-  kill "$pid"
-fi
+count=0
+while [ "$count" -lt 10 ]; do
+    if [ "$(lsof -i "tcp:${SERVER_PORT}" | grep -q LISTEN; echo $?)" != "0" ]; then
+        break
+    fi
+    echo "Waiting for port ${SERVER_PORT} to be free."
+    pkill -f "petclinic.*${SERVER_PORT}"
+    sleep 1
+    count=$((count + 1))
+done
 
 echo "Deploying at port: $SERVER_PORT"
-java -jar ../dist/*.jar --server.port=$SERVER_PORT & sleep 5
+java -jar ../dist/*.jar --server.port=$SERVER_PORT & disown
+
+sleep 10
